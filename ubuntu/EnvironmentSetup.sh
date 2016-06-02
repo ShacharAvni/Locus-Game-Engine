45ee11ffa09e60aef7245ec3b847f2db59f67892
ShacharAvni <shac.avni@gmail.com>
2016-06-01 20:29:45 -0400

    Added Ubuntu helper scripts and a cheat sheet for setting up multiple GCC versions

---
+++ ubuntu/EnvironmentSetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install build-essential
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install g++-4.9
---
+++ ubuntu/GrabDependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get install libgl1-mesa-dev
+sudo apt-get install libopenal-dev
+sudo apt-get install libxrandr-dev
+sudo apt-get install libxi-dev
